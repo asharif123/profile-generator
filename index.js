404e5dadfae83bdb2dce863006c59c15b25564e5
Awad Sharif <awadsharif9@gmail.com>
2021-11-28 12:39:47 -0800

    updated the switch statements in index.js

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-const employee = require("./lib/Employee");
-const engineer = require("./lib/Engineer");
-const intern = require("./lib/Intern");
-const manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
 
 // build a Node.js command-line application that takes in information about employees on a software engineering team, 
 // then generates an HTML webpage that displays summaries for each person
@@ -43,6 +42,10 @@ const manager = require("./lib/Manager");
 const allEmployees = [];
 
 const createEmployee = () => {
+// used to store if user selected either Manager, Engineer or Intern
+    let role = ''
+// used to store the input responses made by user so that we can create our corresponding classes
+    let basicData;
     inquirer.prompt([
 
         {
@@ -75,6 +78,8 @@ const createEmployee = () => {
 
 // generate specific question based off user selection
     ]).then(response => {
+        role = response.selection
+        basicData = response
         inquirer.prompt([
             {
                 when: () => response.selection === "Manager",
@@ -107,49 +112,95 @@ const createEmployee = () => {
 
         
         .then((results) => {
+            // variable to store each employee we create in new class
+            let newEmployee;
+            switch (role) {
+                case "Manager":
+                    newEmployee = new Manager(basicData.title,basicData.ID,basicData.email,results.officeNumber);
+                    break;
 
-    //***** if user selects yes, allow user to add another employee while adding previously created employees*****// 
+                case "Engineer":
+                    newEmployee = new Engineer(basicData.title,basicData.ID,basicData.email,results.github);
+                    break;
+
+                case "Intern":
+                    newEmployee = new Intern(basicData.title,basicData.ID,basicData.email,results.school);
+                    break;
+            
+                default:
+                    break;
+            }
+// push all the responses the user inputted in the all employees array
+            console.log("newEmployee", newEmployee);
+            allEmployees.push(newEmployee);
+// call the createEmployee() function to allow user to add more employees
+            console.log("ALL EMPLOYEES", allEmployees);
+
+    //***** if user selects yes, allow user to add another employee while adding previously selected employees*****// 
             if (results.addEmployee === "Y" || results.addEmployee === "y") {
-
-    // remove add employee key from 'results' object as this info is not needed to add to HTML
-                delete results["addEmployee"];
-
-    // take each item in results and add to response object
-    //NOTE: response contains the answers user inputted AND
-    //results contains user inputs based off specific selection of either manager,engineer or intern
-    //the purpose of this is to combine into one object to include either officenumber, github or school name options
-    //based off if user selected either engineer, manager, or intern
-                for (item in results) {
-                    response[item] = results[item];
-                }
-
-    // push all the responses the user inputted in the all employees array
-                
-                allEmployees.push(response);
                 createEmployee();
             }
 
     //***** if user selects no and does not want to add anymore employees do the following *****//
-    //***** do for each on each employee in allEmployees database and add to corresponding class (Engineer,Manager,or Intern) based of selection key  */ 
+    //***** add all the created employees to the allEmployee database */
+    //***** do for each method on each employee in allEmployees database and add to corresponding class (Engineer,Manager,or Intern) based of selection key  */ 
     //***** run the generateHTML function based off   */
             else {
-
-    // remove add employee key from 'results' object as this info is not needed to add to HTML
-                delete results["addEmployee"];
-
-    // take each item in results and add to response object
-    //NOTE: response contains the answers user inputted AND
-    //results contains user inputs based off specific selection of either manager,engineer or intern
-    //the purpose of this is to combine into one object to include either officenumber, github or school name
-                for (item in results) {
-                    response[item] = results[item];
-                }
-
-    // push all the responses the user inputted in the all employees array 
-                allEmployees.push(response);
-                console.log("***ALL EMPLOYEES*****", allEmployees);
-        
-        }
+                console.log("EMPLOYEES", allEmployees);
+                
+                const templateHTML = `
+                <!DOCTYPE html>
+                <html>
+                  <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1">
+                    <title>Mythical Creatures Zoo</title>
+                    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
+                  </head>
+                  <body>
+                    <section class="section">
+                        <div class="container">
+                        <h1 class="title">
+                            My Team
+                        </h1>
+                        </br>
+                        </br>
+                        <div class="columns is-multiline">
+                            ${allEmployees.map(value => {
+                                return `
+                                <div class="column is-4">
+                                    <div class="card">
+                                        <div class="card-content">
+                                            <div class="content">
+                                                <h3> Name: ${value.title}</h3>
+                                                <p>  Role: ${value.getRole()} </p>
+                                                <p> ID: ${value.id} </p>
+                                                <p> Email: ${value.email} </p>
+                                                ${
+                                                    () => { 
+                                                    if (value.getRole() === "Manager") {
+                                                        return `<p>Office Number: ${value.getOfficeNumber()}</p>`
+                                                    } else if (value.getRole() === "Engineer") {
+                                                        return `<p>GitHub: ${value.getGitHub()}</p>`
+                                                    } else if (value.getRole() === "Intern") {
+                                                        return `<p>School: ${value.getSchool()}</p>`
+                                                    } else {
+                                                        return ""
+                                                    }
+                                                    }
+                                                }                                    
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>`
+                            })}
+                        </div>
+                    </section>
+                  </body>
+                </html>
+                `
+            // fs writefile with that html as index.html
+            }
         })
     .catch((error) => (console.log(error)));
     })
