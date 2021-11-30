3973da3e91e6ab4abe0653b0628164ee7549f9d7
Awad Sharif <awadsharif9@gmail.com>
2021-11-29 19:55:37 -0800

    added styles.css file

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ const createEmployee = () => {
             name: 'selection'
         }
 
-// generate specific question based off user selection
+// generate specific question based off above user selection
     ]).then(response => {
         role = response.selection
         basicData = response
@@ -114,27 +114,30 @@ const createEmployee = () => {
         .then((results) => {
             // variable to store each employee we create in new class
             let newEmployee;
-            switch (role) {
+            switch (response.selection) {
                 case "Manager":
                     newEmployee = new Manager(basicData.title,basicData.ID,basicData.email,results.officeNumber);
+                    console.log("NEW EMPLOYEE", newEmployee.getRole());
                     break;
 
                 case "Engineer":
-                    newEmployee = new Engineer(basicData.title,basicData.ID,basicData.email,results.github);
+                    newEmployee = new Engineer(response.title,response.ID,response.email,results.github);
+                    console.log("NEW ROLE", newEmployee.getRole());
                     break;
 
                 case "Intern":
-                    newEmployee = new Intern(basicData.title,basicData.ID,basicData.email,results.school);
+                    newEmployee = new Intern(response.title,response.ID,response.email,results.school);
+                    console.log("NEW ROLE", newEmployee.getRole());
                     break;
             
                 default:
                     break;
             }
+            
 // push all the responses the user inputted in the all employees array
-            console.log("newEmployee", newEmployee);
             allEmployees.push(newEmployee);
 // call the createEmployee() function to allow user to add more employees
-            console.log("ALL EMPLOYEES", allEmployees);
+            // console.log("ALL EMPLOYEES", allEmployees);
 
     //***** if user selects yes, allow user to add another employee while adding previously selected employees*****// 
             if (results.addEmployee === "Y" || results.addEmployee === "y") {
@@ -146,60 +149,66 @@ const createEmployee = () => {
     //***** do for each method on each employee in allEmployees database and add to corresponding class (Engineer,Manager,or Intern) based of selection key  */ 
     //***** run the generateHTML function based off   */
             else {
-                console.log("EMPLOYEES", allEmployees);
-                
+    // create a template to take employees from allEmployees database and add to our custon HTML
                 const templateHTML = `
                 <!DOCTYPE html>
                 <html>
                   <head>
                     <meta charset="utf-8">
                     <meta name="viewport" content="width=device-width, initial-scale=1">
-                    <title>Mythical Creatures Zoo</title>
-                    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
-                  </head>
+                    <title>Employee Generator</title>
+                    <link href="/assets/styles.css" rel="stylesheet" />
+                    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+                    <link rel="preconnect" href="https://fonts.googleapis.com">
+                    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+                    <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@500&family=Lato:ital,wght@1,700&family=Montserrat&family=Open+Sans:wght@500&family=Roboto:wght@500&display=swap" rel="stylesheet">                  </head>
                   <body>
                     <section class="section">
                         <div class="container">
-                        <h1 class="title">
-                            My Team
-                        </h1>
-                        </br>
-                        </br>
-                        <div class="columns is-multiline">
-                            ${allEmployees.map(value => {
-                                return `
-                                <div class="column is-4">
-                                    <div class="card">
-                                        <div class="card-content">
-                                            <div class="content">
-                                                <h3> Name: ${value.title}</h3>
-                                                <p>  Role: ${value.getRole()} </p>
-                                                <p> ID: ${value.id} </p>
-                                                <p> Email: ${value.email} </p>
-                                                ${
-                                                    () => { 
-                                                    if (value.getRole() === "Manager") {
-                                                        return `<p>Office Number: ${value.getOfficeNumber()}</p>`
-                                                    } else if (value.getRole() === "Engineer") {
-                                                        return `<p>GitHub: ${value.getGitHub()}</p>`
-                                                    } else if (value.getRole() === "Intern") {
-                                                        return `<p>School: ${value.getSchool()}</p>`
-                                                    } else {
-                                                        return ""
-                                                    }
-                                                    }
-                                                }                                    
+                            <h1 class="title">
+                                The A Team!
+                            </h1>
+                            </br>
+                            </br>
+                            <div class="columns is-multiline">
+                                ${allEmployees.map(value => {
+                                    // take each value in allEmployees and map to card element
+                                    console.log("VALUE ROLE", value.getRole())
+                                    return `
+                                    <div class="d-flex justify-content-center section">
+                                        <div class="rounded col-lg-6 col-md-6 col-sm-12 p-2 card">
+                                            <div class="card-content">
+                                                <div class="content">
+                                                    <h3> Name: ${value.name}</h3>
+                                                    <h6>  Role: ${value.getRole()} </h6>
+                                                    <p> ID: ${value.id} </p>
+                                                    <p> Email: ${value.email} </p>
+                                                    ${(
+                                                        () => { 
+                                                        if (value.getRole() === "Manager") {
+                                                            return `<p>Office Number: ${value.getOfficeNumber()}</p>`
+                                                        } else if (value.getRole() === "Engineer") {
+                                                            return `<a href="https://github.com/${value.getGitHub()}"><p class="github">Github</p> </a>`
+                                                        } else if (value.getRole() === "Intern") {
+                                                            return `<p>School: ${value.getSchool()}</p>`
+                                                        } else {
+                                                            return ""
+                                                        }
+                                                        })
+                                                    ()}                                    
+                                                </div>
                                             </div>
                                         </div>
-                                    </div>
-                                </div>`
-                            })}
+                                    </div>`
+                                })}
+                            </div>
                         </div>
                     </section>
                   </body>
                 </html>
                 `
             // fs writefile with that html as index.html
+            fs.writeFileSync("index.html", templateHTML);
             }
         })
     .catch((error) => (console.log(error)));
